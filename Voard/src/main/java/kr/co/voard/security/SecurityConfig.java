a2e736f6206c47200f2387ab9a3db2d982205436
imminji000 <swlss79@naver.com>
2023-04-17 12:04:55 +0900

    2023/04/17

--- Voard/src/main/java/kr/co/voard/security/SecurityConfig.java
+++ Voard/src/main/java/kr/co/voard/security/SecurityConfig.java
@@ -13,13 +13,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import kr.co.voard.jwt.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
 	@Autowired
 	private SecurityUserService userService;
+	
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
 		
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,9 +35,9 @@ public class SecurityConfig {
 		.csrf().disable()		// 브라우저를 사용하는 환경이 아니기 때문에 비활성
 		.formLogin().disable() 	// 폼 로그인 방식 비활성
 		.sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS); // JWT 인증을 사용하기 때문에 Session을 끔
-		//.and()
-		//.addFilterBefore(null, null);
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT 인증을 사용하기 때문에 Session을 끔
+		.and()
+		.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		
 		return http.build();
 	}
