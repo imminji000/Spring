84ca9542c6bf1e1c08e12f1145db1d7793cb855a
imminji000 <swlss79@naver.com>
2023-04-27 12:50:10 +0900

    2023/03/27

--- Voard/src/main/java/kr/co/voard/controller/UserController.java
+++ Voard/src/main/java/kr/co/voard/controller/UserController.java
@@ -15,16 +15,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.voard.jwt.JWTUtil;
 import kr.co.voard.repository.UserEntity;
 import kr.co.voard.security.MyUserDetails;
 import kr.co.voard.security.SecurityUserService;
+import kr.co.voard.service.UserService;
+import kr.co.voard.vo.TermsVO;
 import kr.co.voard.vo.UserVO;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-@Controller
+@RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
 
@@ -37,7 +40,25 @@ public class UserController {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
-	@ResponseBody
+	@Autowired
+	private UserService service;
+	
+	
+	@GetMapping("/user/terms")
+	public TermsVO terms() {		
+		return service.selectTerms();
+	}
+	
+	@PostMapping("/user/register")
+	public void register(@RequestBody UserVO vo) {
+		service.insertUser(vo);
+	}
+	
+	@GetMapping("/user/countUid")
+	public int countUid(String uid) {
+		return service.countUid(uid);
+	}
+	
 	@PostMapping("/user/login")
 	public Map<String, Object> login(@RequestBody UserVO vo) {
 		log.info("vo : " + vo);
@@ -66,14 +87,13 @@ public class UserController {
 		return resultMap;		
 	}
 	
-	@ResponseBody
 	@GetMapping("/user/auth")
 	public Map<String, Object> auth(Authentication authentication) {
 		
 		log.info("auth...1");
 		
 		// Security 사용자 인증 객체 
-		MyUserDetails myUserDetails = (MyUserDetails) authentication.getDetails();
+		MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
 		UserEntity user = myUserDetails.getUser();
 		
 		log.info("auth...3 : " + user);
